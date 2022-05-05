a32331d9c4c1a0484eb76469a44ae32578bae6ab
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-05-05 17:03:50 +0200

    tabella pivot tra tag e post (migration e seeder)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(TagSeeder::class);
         $this->call(UserSeeder::class);
+        $this->call(TagSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(PostSeeder::class);
     }
--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Seeder;
 use App\Post;
 use App\Category;
+use App\Tag;
+use Illuminate\Support\Str;
 use Faker\Generator as Faker;
 class PostSeeder extends Seeder
 {
@@ -15,15 +17,22 @@ class PostSeeder extends Seeder
     {
         $categories = Category::all();
         $categoriesId = $categories->pluck('id')->all();
+        $tags = Tag::All();
+        $tagsId = $tags->pluck('id')->all();
 
         for( $i=0; $i<100; $i++ ){
             $post = new Post;
             $post->title = $faker->words(5,true);
             $post->slug = Str::slug($post->title); //metodo statico della classe stringa per concatenare '-' tra le parole
             $post->content = $faker->paragraphs(10, true);
-            $post->published_at = $faker->randomElement([ null, $faker->dateTime() ]);
+            $post->published_at = $faker->randomElement([ null, $faker->dateTime() ]); //$faker->optional()->dateTime()
             $post->category_id = $faker->optional()->randomElement( $categoriesId );
+
+            $randomTags = $faker->randomElements( $tagsId,$faker->numberBetween(1, 3));
+            
             $post->save();
+            
+            $post->tags()->attach($randomTags);
         }
     }
 }
